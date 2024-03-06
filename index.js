b4e82af9dc757f48f31b15d26550a9564c5e2075
mo1230 <1144898671@qq.com>
2024-03-06 23:59:46 +0800

    搭建框架

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const req = require("request");
 const pup = require("puppeteer");
-const $ = require("cheerio");
+const cheerio = require("cheerio");
 
 // 要获取的对象
 
 let opencv_data = {
-    url: "",
+    url: "https://www.w3school.com.cn/js/js_object_methods.asp",
     title: "",
 };
+
+
+// 返回html
+function get_html(url) {
+    req.get(url, (err, resp, body)=>{
+        if (body) {
+            
+            console.log("get_html success!!!");
+            let $ = cheerio.load(body.toString());
+            let els = [];
+            console.log($("h1").text());;
+            return ;
+        }else{
+            console.log("get_html failed!!!");
+        }
+    });
+}
+
+
+// 生成pdf
+function to_pdf(html, title) {
+    
+}
+
+
+function main() {
+    get_html(opencv_data.url);
+}
+main();
