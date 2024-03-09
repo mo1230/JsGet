f64fa550e17c72dc73e00c55d49ea61cc8bf61b8
mo1230 <1144898671@qq.com>
2024-03-10 00:06:30 +0800

    更换第三方库

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const req = require("request");
-const pup = require("puppeteer");
+const req = require("axios").default;
+
 const cheerio = require("cheerio");
-// const { data } = require("cheerio/lib/api/attributes");
+const officegen = require('officegen');
+const fs = require('fs');
+const docx = officegen('docx');//word
+const pptx = officegen('pptx');//pptx
 
 // 要获取的对象
 
@@ -17,36 +20,28 @@ let opencv = {
 
 // 返回html
 function get_html(goal) {
-    req.get(opencv.url, (err, resp, body) => {
-        if (body) {
+    req.get(opencv.url).then((respose)=>{
+        console.log("get_html success!!!");
+        let $ = cheerio.load(respose.data);
+        let datas = [];
+        let data_el = {
+            title: "",
+            href: ""
+        };
+        let titles = $(opencv.selectors.titles);
 
-            console.log("get_html success!!!");
-            let $ = cheerio.load(body.toString());
-            let datas = [];
-            let data_el = {
-                title: "",
-                href: ""
-            };
-            let titles = $(opencv.selectors.titles);
+        for (const i in titles) {
 
-            for (const i in titles) {
-
-                // console.log(typeof(titles[i].attribs));
-                if (typeof (titles[i].attribs) == "object" && Object.getOwnPropertyNames(titles[i].attribs).length != 0) {
-                    // console.log(titles[i].attribs);
-                    data_el.title = titles[i].attribs.title;
-                    data_el.href = titles[i].attribs.href;
-                    datas.push(data_el);
-                    console.log(data_el);
-                }
-
+            // console.log(typeof(titles[i].attribs));
+            if (typeof (titles[i].attribs) == "object" && Object.getOwnPropertyNames(titles[i].attribs).length != 0) {
+                // console.log(titles[i].attribs);
+                data_el.title = titles[i].attribs.title;
+                data_el.href = titles[i].attribs.href;
+                datas.push(data_el);
+                console.log(data_el);
             }
+            // #pro-mian
 
-            // console.log($(opencv.selectors.titles));
-            // console.log(titles);
-            return;
-        } else {
-            console.log("get_html failed!!!");
         }
     });
 }
@@ -61,4 +56,7 @@ function to_pdf(html, title) {
 function main() {
     get_html(opencv);
 }
+
+
+
 main();
