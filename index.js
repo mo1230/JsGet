0ece63100ea81a0d562bb3985e8b60d7538174e7
mo1230 <1144898671@qq.com>
2024-03-11 00:01:19 +0800

    获取所有title的标题和链接

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const req = require("axios").default;
 const cheerio = require("cheerio");
 const officegen = require('officegen');
 const fs = require('fs');
+const { default: axios } = require("axios");
 const docx = officegen('docx');//word
 const pptx = officegen('pptx');//pptx
+const markdown = require("node-html-markdown").NodeHtmlMarkdown;
+const markdown_opt = require("node-html-markdown").NodeHtmlMarkdownOptions;
+
 
 // 要获取的对象
 
@@ -24,25 +28,48 @@ function get_html(goal) {
         console.log("get_html success!!!");
         let $ = cheerio.load(respose.data);
         let datas = [];
-        let data_el = {
-            title: "",
-            href: ""
-        };
+        
+        // 处理url字符串，拼接字符串形成目标url
+        let url_tmp = opencv.url.split("/");
+        let base_url = url_tmp[0] + "//"  + url_tmp[2];
         let titles = $(opencv.selectors.titles);
 
         for (const i in titles) {
-
+            // console.log(i);
             // console.log(typeof(titles[i].attribs));
             if (typeof (titles[i].attribs) == "object" && Object.getOwnPropertyNames(titles[i].attribs).length != 0) {
                 // console.log(titles[i].attribs);
+                let data_el = {
+                    title: "",
+                    href: ""
+                };
                 data_el.title = titles[i].attribs.title;
                 data_el.href = titles[i].attribs.href;
                 datas.push(data_el);
-                console.log(data_el);
+                
+                // console.log(datas);
             }
-            // #pro-mian
-
         }
+        // console.log(datas);
+        for (const el of datas) {
+            if (typeof(el.href) != "undefined") {
+                // console.log(base_url + el.href);
+                el.href = base_url + el.href;
+                // console.log(el.href);
+                setTimeout(() => {
+                    axios.get(el.href).then((respose)=>{
+                        $ = cheerio.load(respose.data);
+                        // let marks = markdown.translate($("pro-mian"));
+                        console.log($("pro-mian"));
+                    });
+                }, Math.random() * 100);
+                
+                // html转为markdown
+                
+                
+            }
+        }
+        
     });
 }
 
